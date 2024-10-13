a923d042b7d2817e9fa2c9e772abb7c6121009f6
Gene Howell <gene.howell9@gmail.com>
2024-10-13 15:26:31 -0500

    Learned about data types and used var_dump to display type and contencts

--- 01-variables-data-types/03-data-types/index.php
+++ 01-variables-data-types/03-data-types/index.php
@@ -2,13 +2,32 @@
 
 /*
   PHP DATA TYPES:
-
-- String
-- Integer
-- Float
-- Boolean
-- Array
-- Object
-- NULL
-- Resource
 */
+
+// - String
+$name = 'Gene Howell';
+$name2 = "John Doe;";
+
+echo '<br />'; // html line break
+
+// - Integer
+$age = 36; // only whole number with no decimal
+
+// - Float
+$rating = 4.5; // only decimal numbers
+
+// - Boolean
+$isLoaded = true; // must be a value of true or false
+
+// - Array
+$friends = ['Jimmy', 'Joe', 'Jane']; // holds a list of items, starting from 0 as an index
+
+// - Object
+$person = new stdClass();
+
+// - NULL
+$car = NULL; // No value
+
+// - Resource
+$file = fopen('sample.txt', 'r'); // opens the textfile within the same directory using the fopen function
+// var_dump($file) // shows the type as a resource
