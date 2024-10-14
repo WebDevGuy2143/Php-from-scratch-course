0ed61a5c89c10bce6e0ed365bb5d0e100688951b
Gene Howell <gene.howell9@gmail.com>
2024-10-13 23:00:37 -0500

    Learned about formatting dates and times

--- 01-variables-data-types/09-working-with-dates/index.php
+++ 01-variables-data-types/09-working-with-dates/index.php
@@ -1,5 +1,55 @@
 <?php
 $output = null;
+/**
+ * 
+ * - `Y` - The year
+ * - `m` - The month
+ * - `d` - The day
+ * - `D` - The day of the week (short name)
+ * - `l` - The full day of the week name
+ * - `h` - The hour
+ * - `i` - The minute
+ * - `s` - The second
+ * - `a` - AM/PM
+ */
+
+
+// Get the Year
+$output = date('Y');
+
+// Get the year with timestamp
+$output = date('Y', 936345600); // outputs the year 1999
+
+// Get timestamp from strtotime
+$output = date('Y', strtotime('1999-09-01')); // also outputs the year 1999
+
+// Get Month
+$output = date('m'); // displays current month
+
+// Get day
+$output = date('D'); // displays shorthand weekday
+$output = date('l'); // displays full weekday
+
+// full date (month, day, and year)
+$output = date('Y - m - d'); // displays full date starting at the year.
+
+$output = date('m - d - Y'); // displays full date, starting with the month
+
+// Get hour
+$output = date('h'); // displays current hour
+
+// Get minute
+$output = date('i'); // displays current minute
+
+// Get second
+$output = date('s'); // displays current second
+
+// Get AM/PM
+$output = date('a'); // displays AM or PM
+
+// Get full date and time
+$output = date('Y-m-d h:i:s a'); // displays full date and time
+
 ?>
 
 <!DOCTYPE html>
