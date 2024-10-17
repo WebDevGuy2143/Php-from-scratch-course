cc9badac8d0f1437debc4347b990265c382d9d7f
Gene Howell <gene.howell9@gmail.com>
2024-10-16 21:56:04 -0500

    learned about associative arrays

--- 02-arrays-and-iteration/03-associative-arrays/index.php
+++ 02-arrays-and-iteration/03-associative-arrays/index.php
@@ -1,5 +1,24 @@
 <?php
 $output = null;
+
+
+// Associative array
+$user = [
+    'name' => 'James',
+    'email' => 'james@gmail.com',
+    'password' => 'pass123',
+    'hobbies' => ['tennis', 'Video Games']
+];
+
+$output = $user['name']; // outputs the value in the key/value pair for the associative array for $users
+
+$output = $user['hobbies'][0]; // searches for the hobbies key and finds the 0 index, in which in this case is the value, tennis
+
+// add to associative array
+$user['address'] = '123 Main Street';
+
+unset($user['address']); // removes associative array
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +41,12 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">User Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($user); ?>
+                </pre>
+            </p>
         </div>
     </div>
 </body>
