5190ef0e9b686f03cbb0d475a6f99766fc1bffb2
Gene Howell <gene.howell9@gmail.com>
2024-10-22 13:20:36 -0500

    Learned basic loops in PHP

--- 02-arrays-and-iteration/06-basic-loops/index.php
+++ 02-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,31 @@
+<?php
+
+// Basic for loop (initializer, condition, incrementor)
+// displays numbers 0-20, per line
+
+/* for ($i = 0; $i <= 20; $i++) {
+    echo $i . '<br />';
+} */
+
+// Basic While Loop. 
+/* $i = 0; // initializer, while function (condition, incrementor), 
+while ($i <= 20) {
+    echo $i . '<br />';
+    $i++;
+} */
+
+// Basic Do-While
+// displays numbers 0-10, but always runs/displays the value of $i no matter what since the compiler sees that first
+// runs twice (displays the value of $i in the echo, then runs the conditional)
+
+/* $i = 0;
+
+do {
+    echo $i;
+    $i++;
+} while ($i < 10); */
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +45,33 @@
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
+            <!-- instead of {} in regular php, within HTML, you use : to open and close off conditionals -->
+            <h1 class="text-3xl font-semibold">Basic Loops</h1><br />
+            <h2 class="text-2xl font-semibold">For Loop</h2>
+            <ul>
+                <?php for ($i = 0; $i <= 20; $i++): ?>
+                    <li>Number: <?= $i ?></li>
+                <?php endfor; ?>
+            </ul>
+
+            <br />
+            <h2 class="text-2xl font-semibold">While Loop</h2>
+            <ul>
+                <?php $i = 0;
+                while ($i <= 20): ?>
+                    <li>Number: <?= $i; ?></li>
+                <?php $i++;
+                endwhile; ?>
+            </ul>
+            <br />
+            <h2 class="text-2xl font-semibold">Do-While</h2>
+            <ul>
+                <?php $i = 0;
+                do { ?>
+                    <li><?= $i; ?></li>
+                <?php $i++;
+                } while ($i <= 10) ?>
+            </ul>
         </div>
     </div>
 </body>
