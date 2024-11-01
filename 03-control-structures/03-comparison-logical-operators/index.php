480cef0abe2f936d7f8247dc94f4858f6b9499e1
Gene Howell <gene.howell9@gmail.com>
2024-11-01 11:28:47 -0500

    Learned about comparison and logical operators

--- 03-control-structures/03-comparison-logical-operators/index.php
+++ 03-control-structures/03-comparison-logical-operators/index.php
@@ -15,6 +15,17 @@
 | >=       | Greater than or equal to |
 */
 
+$x = 10;
+$y = '10';
+
+// var_dump($x == $y); // returns true becuase the double equals are used to compare values
+// var_dump($x === $y); // returns false becuase the tripple equals are used to compare types of values
+// var_dump($x != $y); // returns false becuase the comparison is not looking at the types of values. <> is also the same.
+// var_dump($x !== $y); // returns true becuase the comparison is looking at the types of values
+// var_dump($x > $y); // returns false becuase the comparison is looking at the types of values, not the values of the variables.
+// var_dump($x < $y); // returns false becuase the comparison is looking at the types of values, not the values of the variables.
+// var_dump($x <= $y); // returns true becuase the comparison is looking at the types of values, not the values of the variables.
+// var_dump($x >= $y); // returns true becuase the comparison is looking at the types of values, not the values of the variables.
 
 /*
 | Logical Operators
@@ -27,3 +38,13 @@
 | xor      | True if one is true    |
 | !        | True if it is not true |
 */
+
+$a = 10;
+$b = 20;
+
+// var_dump($a == 10 && $b == 20); // returns true
+// var_dump($a == 10 and $b == 20); // returns true
+// var_dump($a == 10 || $b == 20); // returns true
+// var_dump($a == 10 or $b == 20); // returns true
+// var_dump($a == 10 xor $b == 20); // returns false
+// var_dump(!$a == 4); // returns true
