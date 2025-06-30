557f0d2640f02581ce9420734c989f20aa3085a9
Gene Howell <gene.howell9@gmail.com>
2025-06-30 12:23:16 -0500

    learn extends, and parent keywords within a class

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,37 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+// Creating a new class that inherits from the User class using the "extends" keyword
+class Admin extends User
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+
+    // Using the parent keyword to call the constructor of the parent class (User)
+    parent::__construct($name, $email);
+  }
+
+  public function login(){
+    echo 'Admin ' . $this->name . ' logged in successfully <br>';
+  }
+
+  public function getStatus(){
+    echo $this->status . '<br>';
+  }
+}
+
+// Creating an instance of the Admin class and passing in arguments
+$admin1 = new Admin('John Doe', 'jdoe@example.com', 5);
+// $admin1->getStatus();
+$admin1->login();
+
+// Accessing the properties of the Admin class
+// echo $admin1->name . '<br>';
+// echo $admin1->email . '<br>';
+// echo $admin1->level . '<br>';
+
+// var_dump($admin1);
